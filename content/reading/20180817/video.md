4441733b6b863a7e3f44fdbe175a11506f2e2384
John Deng <john.deng@qq.com>
2018-12-04 15:39:58 +0800

    added growingio script

--- content/reading/20180817/video.md
+++ content/reading/20180817/video.md
@@ -4,4 +4,4 @@ title:  sync.Pool 源码分析及适用场景
 
 ## 观看视频
 
-{{< youtube id="jaepwn2PWPk" >}} 
+{{< youtube id="jaepwn2PWPk" >}}
---
+++ themes/hugo-material-docs/static/javascripts/growingio.js
@@ -0,0 +1,13 @@
+var _vds = _vds || [];
+      window._vds = _vds;
+      (function(){
+        _vds.push(['setAccountId', 'b5ceca26ced64330bc071c17f1bc12fa']);
+        (function() {
+          var vds = document.createElement('script');
+          vds.type='text/javascript';
+          vds.async = true;
+          vds.src = ('https:' == document.location.protocol ? 'https://' : 'http://') + 'assets.growingio.com/vds.js';
+          var s = document.getElementsByTagName('script')[0];
+          s.parentNode.insertBefore(vds, s);
+        })();
+      })();
