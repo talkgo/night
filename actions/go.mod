19d16e226454e94e565f766b682db19fedf513fd
soyum2222 <shu123green@qq.com>
2020-09-03 15:25:22 +0800

    auto issues

---
+++ actions/go.mod
@@ -0,0 +1,9 @@
+module night/actions
+
+go 1.15
+
+require (
+	github.com/dyweb/gommon v0.0.13
+	github.com/google/go-github/v29 v29.0.3
+	golang.org/x/oauth2 v0.0.0-20200902213428-5d25da1a8d43
+)
---
+++ actions/weekly.go
@@ -0,0 +1,68 @@
+package main
+
+import (
+	"context"
+	"encoding/json"
+	"flag"
+	"fmt"
+	"github.com/dyweb/gommon/util/httputil"
+	"github.com/google/go-github/v29/github"
+	"golang.org/x/oauth2"
+	"io/ioutil"
+	"os"
+)
+
+type Issue struct {
+	Title    string   `json:"title"`
+	Body     string   `json:"body"`
+	Labels   []string `json:"labels"`
+	Assignee []string `json:"assignee"`
+}
+
+func main() {
+
+	var cfg string
+
+	flag.StringVar(&cfg, "c", "", "")
+	flag.Parse()
+
+	data, err := ioutil.ReadFile(cfg)
+	if err != nil {
+		panic(err)
+	}
+
+	issueInfo := Issue{}
+	err = json.Unmarshal(data, &issueInfo)
+	if err != nil {
+		panic(err)
+	}
+
+	var token = os.Getenv("GITHUB_TOKEN")
+
+	hc := httputil.NewUnPooledClient()
+	if token != "" {
+		ts := oauth2.StaticTokenSource(&oauth2.Token{
+			AccessToken: token,
+		})
+		hc = oauth2.NewClient(context.Background(), ts)
+	}
+
+	client := github.NewClient(hc)
+
+	req := &github.IssueRequest{
+		Title:     &issueInfo.Title,
+		Labels:    &issueInfo.Labels,
+		Assignees: &issueInfo.Assignee,
+		Body:      &issueInfo.Body,
+	}
+
+	issue, _, err := client.Issues.Create(context.Background(), "talkgo", "night", req)
+	if err != nil {
+		fmt.Println(err)
+	}
+	if err != nil {
+		panic(err)
+	}
+	fmt.Println("Created new issue %d %s", issue.GetNumber(), issue.GetTitle())
+
+}
